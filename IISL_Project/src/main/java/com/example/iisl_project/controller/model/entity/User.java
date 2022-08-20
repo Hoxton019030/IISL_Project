bde8efb4a358c7a4c8fbce81468ae131d1a84356
THINK-YICELWEN\yiwen <za546321@yahoo.com.tw>
2022-08-20 14:13:38 +0800

    h2 fail

---
+++ IISL_Project/src/main/java/com/example/iisl_project/controller/model/entity/User.java
@@ -0,0 +1,18 @@
+//package com.example.iisl_project.controller.model.entity;
+//
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//
+//@Entity
+//public class User {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    Integer id;
+//
+//    @
+//
+//
+//}
---
+++ IISL_Project/src/main/resources/schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS user(
+    name varchar(20),
+    age int,
+    birthday date);
