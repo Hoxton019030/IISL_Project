8777e4c8feeee1a64b04f2ebedea047345e8d215
DESKTOP-RBMVNA4\Hoxton <hoxton.hsu@eunodata.com>
2022-08-23 09:44:38 +0800

    feat:<TestController> create login

--- src/main/java/com/example/iisl_project/controller/TestController.java
+++ src/main/java/com/example/iisl_project/controller/TestController.java
@@ -1,12 +1,24 @@
 package com.example.iisl_project.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.iisl_project.util.JwtToken;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RestController
 public class TestController {
     @RequestMapping("/")
     public String helloWorld(){
         return "Hello World";
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody HashMap<String,String> user){
+        JwtToken jwtToken = new JwtToken();
+        String token = jwtToken.generateToken(user);
+        return ResponseEntity.status(HttpStatus.OK).body(token);
     }
 }
--- src/main/java/com/example/iisl_project/util/JwtToken.java
+++ src/main/java/com/example/iisl_project/util/JwtToken.java
@@ -19,12 +19,22 @@ public class JwtToken implements Serializable {
 
     public static final String SECRET = "1337";
 
+    /**
+     *產生JwtToken
+     * @param userDetails 使用者登入資訊 帳號密碼之類的吧
+     * @return JwToken 的加密字串
+     */
     public String generateToken(HashMap<String,String> userDetails){
         HashMap<String, Object> claims = new HashMap<>();
         claims.put("userName",userDetails.get("userName"));
         return Jwts.builder().setClaims(claims).setExpiration(new Date(Instant.now().toEpochMilli()+EXPIRATION_TIME)).signWith(SignatureAlgorithm.HS512,SECRET).compact();
     }
 
-
-
+    /**
+     * 經由generateToken所產生的金鑰
+     * @param token JwToken的加密字串
+     */
+    public void validateToken(String token){
+        Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token);
+    }
 }
